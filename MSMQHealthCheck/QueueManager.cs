4cdcb2cbc8497bcb6d1696a259ce8426d7e71c14
yayayahei <mmwcbz@msn.cn>
2019-08-30 13:38:59 +0800

    add QueueManager

---
+++ MSMQHealthCheck/QueueManager.cs
@@ -0,0 +1,12 @@
+﻿namespace MSMQHealthCheck
+{
+    public class QueueManager
+    {
+        private readonly string _pathName;
+
+        public QueueManager(string pathName)
+        {
+            _pathName = pathName;
+        }
+    }
+}

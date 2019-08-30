b877913565fb95e85aea7e1618660e99075bdc32
yayayahei <mmwcbz@msn.cn>
2019-08-30 14:04:39 +0800

    check queue existance

--- MSMQHealthCheck/QueueManager.cs
+++ MSMQHealthCheck/QueueManager.cs
@@ -1,12 +1,32 @@
-﻿namespace MSMQHealthCheck
+﻿using System.Messaging;
+
+namespace MSMQHealthCheck
 {
+    /// <summary>
+    /// manage a queue
+    /// </summary>
     public class QueueManager
     {
         private readonly string _pathName;
+        public MessageQueue MessageQueue { get; set; }
 
         public QueueManager(string pathName)
         {
             _pathName = pathName;
+
+            if (Exist())
+            {
+                MessageQueue = new MessageQueue(_pathName);
+            }
+        }
+
+        /// <summary>
+        /// check the queue exists or not
+        /// </summary>
+        /// <returns></returns>
+        public bool Exist()
+        {
+            return MessageQueue.Exists(_pathName);
         }
     }
 }
